5c363c07f2264ba42556b9d2d6cf17e3bc45802d
luvie23 <luvierallos@gmail.com>
2022-12-17 15:04:27 +0800

    playing around with the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,18 +6,15 @@ import heroImg from "../assets/Hero.JPG"
 
 const Hero = () => {
     return (
-        <section className="h-screen bg-gradient-to-tl from-gray-300 to-gray-900 flex flex-auto">
-            <div className="w-1/2 flex justify-center items-center font-extrabold">
-                Front-End Developer
-
-            </div>
-
-            <div className="w-1/2 flex justify-center items-center">
+        <section className="h-screen w-full bg-gradient-to-tl from-gray-300 to-gray-900 flex flex-col items-center font-Roboto pt-48 px-10 text-4xl md:text-5xl">
                 
-                    <img src={heroImg} className=" w-2/3 rounded-lg"/>
+            <span className="w-full  md:w-2/3 font-bold">Luvie Mar Rallos. </span>
+            <span className="pl-5 w-full md:w-2/3 font-normal text-xl text-slate-200">Web Developer</span>
+            <span className="w-full pt-5 font-normal md:w-2/3 italic">I create things on the internet using modern web technologies.
+            </span>
 
-                
-            </div>   
+       
+
         </section>
     )
 }
