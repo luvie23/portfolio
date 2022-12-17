1578e434dfa5d49f16edfc52f9c56cebd317144d
luvie23 <luvierallos@gmail.com>
2022-12-17 15:48:52 +0800

    start on Navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 function App() {
   return (
     <div >
+        <Navbar/>
         <Hero/>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,20 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav>
-
+        <nav >
+            <ul className="flex gap-2 justify-end mr-10">
+                <li>
+                    github
+                </li>
+                <li>
+                    linkedin
+                </li>
+                <li>
+                    contact   
+                </li>
+                
+                
+            </ul>
         </nav>
     )
 }
